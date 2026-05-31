be5c7ce2387c33e391e387092adc9a892a5158da
Evan Bowling <evan.j.bowling@gmail.com>
2026-05-31 15:08:38 -0500

    feat: update index.md with presentations

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,17 @@ GIT_TOP=$(git rev-parse --show-toplevel)
 
 update_index() {
   local INDEX="$GIT_TOP/index.md"
+
   printf "# Articles\n\n" > "$INDEX"
   for a in $(git -C "$GIT_TOP" ls-files articles/ | sed 's|articles/||' | cut -d'/' -f1 | sort -ru); do
     echo "- [${a}](articles/${a})" >> "${INDEX}"
   done
+
+  printf "\n# Presentations\n\n" >> "$INDEX"
+  for p in $(git -C "$GIT_TOP" ls-files presentations/ | sed 's|presentations/||' | cut -d'/' -f1 | sort -ru); do
+    echo "- [${p}](presentations/${p})" >> "${INDEX}"
+  done
+
   printf "\n" >> "${INDEX}"
 }
 
