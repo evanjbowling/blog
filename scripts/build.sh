b01f5920e332955331b12c55bddca546cb6bda32
Evan Bowling <evan.j.bowling@gmail.com>
2026-05-31 11:53:43 -0500

    feat: Small update to index.md build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ GIT_TOP=$(git rev-parse --show-toplevel)
 
 update_index() {
   local INDEX="$GIT_TOP/index.md"
-  printf "# blog\n\n" > "$INDEX"
+  printf "# Articles\n\n" > "$INDEX"
   for a in $(git -C "$GIT_TOP" ls-files articles/ | sed 's|articles/||' | cut -d'/' -f1 | sort -ru); do
     echo "- [${a}](articles/${a})" >> "${INDEX}"
   done
